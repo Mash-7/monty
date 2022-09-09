5c02492a812c678c132e23b729dc903e299a7505
Mash <104835532+Mash-7@users.noreply.github.com>
2022-09-09 04:34:16 +0200

    update opcodes

--- get_opcodes.c
+++ get_opcodes.c
@@ -2,39 +2,39 @@
 
 /**
  * get_opcodes - selects the correct opcode to execute
- * @opc: opcode passed
+ * @opcode: opcode passed
  *
  * Return: pointer to the function that executes the opcode
  */
-void (*get_opcodes(char *opc))(stack_t **stack, unsigned int line_number)
+void (*get_opcodes(char *opcode))(stack_t**, unsigned int)
 {
 	instruction_t instruct[] = {
-		{"push", _push},
-		{"pall", _pall},
-		{"pint", _pint},
-		{"pop", _pop},
-		{"swap", _swap},
-		{"queue", _queue},
-		{"stack", _stack},
-		{"add", _add},
-		{"nop", _nop},
-		{"sub", _sub},
-		{"mul", _mul},
-		{"div", _div},
-		{"mod", _mod},
-		{"pchar", _pchar},
-		{"pstr", _pstr},
-		{"rotl", _rotl},
-		{"rotr", _rotr},
+		{"push", m_push},
+		{"pall", m_pall},
+		{"pint", m_pint},
+		{"pop", m_pop},
+		{"swap", m_swap},
+		{"queue", m_queue},
+		{"stack", m_stack},
+		{"add", m_add},
+		{"nop", m_nop},
+		{"sub", m_sub},
+		{"mul", m_mul},
+		{"div", m_div},
+		{"mod", m_mod},
+		{"pchar", m_pchar},
+		{"pstr", m_pstr},
+		{"rotl", m_rotl},
+		{"rotr", m_rotr},
 		{NULL, NULL}
 	};
 	int o;
 
-	for (o = 0; instruct[o].opcode; o++)
+	for (o = 0; op_funcs[o].opcode; o++)
 	{
-		if (_strcmp(instruct[o].opcode, opc) == 0)
-			break;
+		if (_strcmp(opcode, op_funcs[o].opcode) == 0)
+			return (op_funcs[o].f);
 	}
 
-	return (instruct[o].f);
+	return (NULL);
 }
