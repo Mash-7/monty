0aa9be4c11310ffd88a52005fcf9f8b45e9a59d9
Mash <104835532+Mash-7@users.noreply.github.com>
2022-09-09 04:36:35 +0200

    update monty.c

--- monty.c
+++ monty.c
@@ -10,12 +10,32 @@ int run_monty(FILE *script_fd);
 /**
  * free_tokens - Frees the global op_toks array of strings.
  */
+void free_tokens(void)
+{
+	size_t ft = 0;
+
+	if (op_toks == NULL)
+		return;
+
+	for (i = 0; op_toks[ft]; ft++)
+		free(op_toks[ft]);
+
+	free(op_toks);
+}
 
 /**
  * token_arr_len - Returns length of current op_toks.
  *
  * Return: Length of current op_toks (as int).
  */
+unsigned int token_arr_len(void)
+{
+	unsigned int len = 0;
+
+	while (op_toks[len])
+		toks_len++;
+	return (len);
+}
 
 /**
  * is_empty_line - Checks if a line only contains delimiters.
@@ -25,4 +45,20 @@ int run_monty(FILE *script_fd);
  * Return: If only delimiters, 1.
  * Else, 0.
  */
+int is_empty_line(char *line, char *delims)
+{
+	int i, j;
 
+	for (i = 0; line[i]; i++)
+	{
+		for (j = 0; delims[j]; j++)
+		{
+			if (line[i] == delims[j])
+				break;
+		}
+		if (delims[j] == '\0')
+			return (0);
+	}
+
+	return (1);
+}
